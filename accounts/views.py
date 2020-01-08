dbdc4dbf8a89b106726d58094cc7cecea77f47e8
AlexMuia31 <alexmuia31@gmail.com>
2020-01-09 00:45:51 +0300

    static files and more

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def signup(request):
+    render(request, 'accounts/signup.html')
+def login(request):
+    render(request, 'accounts/login.html')
+def logout(request):
+    render(request, 'accounts/signup.html')
