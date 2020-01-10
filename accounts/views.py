f673ee18e57cba99c2112990760c92a72f10b895
AlexMuia31 <alexmuia31@gmail.com>
2020-01-11 01:02:24 +0300

    sign up completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
-    render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password1']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html',{'error': 'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html',{'error': 'Password must match'})
+              
+    else:
+         
+        return render(request, 'accounts/signup.html')
+
 def login(request):
-    render(request, 'accounts/login.html')
+    return render(request, 'accounts/login.html')
+
 def logout(request):
-    render(request, 'accounts/signup.html')
+    return render(request, 'accounts/signup.html')
