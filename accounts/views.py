e1d176517762b2c8015392e1b6d9430bbd93d8cb
AlexMuia31 <alexmuia31@gmail.com>
2020-01-12 00:00:20 +0300

    login and logout set up succesfully

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.shortcuts import redirect
 
 def signup(request):
     if request.method == 'POST':
@@ -14,13 +15,26 @@ def signup(request):
                 return redirect('home')
         else:
             return render(request, 'accounts/signup.html',{'error': 'Password must match'})
-              
+               
     else:
          
         return render(request, 'accounts/signup.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username= request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html',{'error':'Username or Password is incorrect.'})
+
+    else:
+        
+        return render(request, 'accounts/login.html')
 
 def logout(request):
+    if request.method =='POST':
+        auth.logout(request)
+        return redirect ('home')
     return render(request, 'accounts/signup.html')
