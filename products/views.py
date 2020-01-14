9c24722b236bc8555f3654ddc9a254a93b794ffd
AlexMuia31 <alexmuia31@gmail.com>
2020-01-14 19:27:30 +0300

    products model and much more

--- products/views.py
+++ products/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
+
 
 def home(request):
     return render(request, 'home.html')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon']and request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url'] 
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('home')
+        else:
+            return render(request, 'create.html', {'error':'All fields are required'})
+                
+    else:
+        return render(request, 'create.html')
