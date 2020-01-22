961e6da97aab045d4ec5ea7fed477bb18257df40
alex <you@example.com>
2020-01-22 19:40:46 +0300

    login required added to eliminate 404 error

--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ def home(request):
     products= Product.objects
     return render(request, 'home.html', {'products':products})
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon']and request.FILES['image']:
@@ -35,7 +35,7 @@ def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'detail.html',{'product':product})
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def upvote(request, product_id):
     if request.method == 'POST':
         product = get_object_or_404(Product, pk=product_id)
